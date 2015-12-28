17e89b99e1c05eeb3d8996dd2a8057cccddfe171
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2015-12-28 04:09:38 -0300

    Discriminación por parte de la ruta /sign-up entre POST y GET. Se añade el método que genera la pass encriptada.

--- qti_interaction/controller.py
+++ qti_interaction/controller.py
@@ -6,8 +6,8 @@ from flask import (
     url_for,
     jsonify)
 
-
 from qti_interaction import app
+import hashlib
 import model
 
 
@@ -18,14 +18,59 @@ def index():
 
 @app.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
-    return render_template('sign-up.html')
+    if request.method == "POST":
+        nombres = (request.form["nombres"]).encode('utf-8')
+        email = (request.form["email"]).encode('utf-8')
+        usuario = (request.form["usuario"]).encode('utf-8')
+        password = (request.form["password"]).encode('utf-8')
+        password2 = (request.form["password2"]).encode('utf-8')
+
+        exito = model.registrar_usuario(nombres,
+                                        email,
+                                        usuario,
+                                        encrypt_pass(usuario,
+                                                     email,
+                                                     password)
+                                        )
+        if exito:
+            return render_template('main_window.html')
+        else:
+            pass
+
+    else:
+        return render_template('sign-up.html')
+
 
 @app.route('/verifica_usuario')
 def verifica_usuario():
-	usuario = request.args.get('usuario', 0, type=str)	
-	respuesta = model.verifica_usuario(usuario)
-	return jsonify(result=respuesta)
+    usuario = request.args.get('usuario')
+    respuesta = model.verifica_usuario(usuario)
+    return jsonify(result=respuesta)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/verifica_email')
+def verifica_email():
+    email = request.args.get('email')
+    respuesta = model.verifica_email(email)
+    return jsonify(result=respuesta)
+
+
+def encrypt_pass(usuario, email, password):
+    """
+    Encripta la contraseña del usuario mediante hashlib, se utiliza sha224.
+    En el proceso de encriptación se utiliza un salt personalizado para
+    cada nuevo usuario.
+    """
+    if len(usuario) < len(email.split('@')[0]):
+        end = len(usuario)
+    else:
+        end = len(email.split('@')[0])
+    salt = ""
+    for i in range(0, end):
+        salt = "{}{}{}".format(salt, usuario[i], email[i])
+
+    hash_pass = hashlib.sha224("{}{}{}".format(salt[0:len(salt) / 2].upper(),
+                                               password,
+                                               salt[len(salt) / 2: len(salt)])
+                               ).hexdigest()
+    return hash_pass
